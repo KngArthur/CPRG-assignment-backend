bdd260cbe51a14f4637a5d26dd45be9427f21436
Branden Arthur <branden.arthur13@gmail.com>
2020-07-25 15:25:26 -0600

    edits from ejs lecture

---
+++ destinations.js
@@ -0,0 +1,29 @@
+const destinations = [
+  {
+    title: 'Tofino', 
+    id: 1019
+  },
+  {
+    title: 'California', 
+    id: 1065
+  },
+  {
+    title: 'Cabin',
+    id: 1051
+  },
+  {
+    title: 'Northshore',
+    id: 1049
+  },
+  {
+    title: 'Waterfall',
+    id: 1035
+  },
+  {
+    title: 'Foothills',
+    id: 166
+  }
+
+];
+
+module.exports = destinations;
